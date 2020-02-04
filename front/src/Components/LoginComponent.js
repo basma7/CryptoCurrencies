3b684f19286d21d2e5e44f3be70800d79a0f822b
Albatard <nicolas.albanel@epitech.eu>
2020-02-04 18:03:58 +0100

    update front

--- front/src/Components/LoginComponent.js
+++ front/src/Components/LoginComponent.js
@@ -6,18 +6,28 @@ import {Form, Button, Container} from 'react-bootstrap';
 export default class LoginComponent extends Component {
   constructor(props) {
     super();
-    // this.state.handlePlayPause = this.state.handlePlayPause.bind(this)
+    this.onLogin = this.onLogin.bind(this)
     this.state = {
+        login: {
+            email: "",
+            password: "",
+          }
     }
 }
-  
+    onLogin(event) {
+        event.preventDefault()
+        this.state.login.email = event.target.email.value
+        this.state.login.password = event.target.password.value
+        console.log(this.state.login)
+  }
+
   render() 
   {
     return (
         <Container className="login-container">
             <h1 className="login-title">Login</h1>
-            <Form>
-                <Form.Group controlId="formBasicEmail">
+            <Form onSubmit={this.onLogin}>
+                <Form.Group controlId="email">
                 <Form.Label>Email address</Form.Label>
                 <Form.Control type="email" placeholder="Enter email" />
                 <Form.Text className="text-muted">
@@ -25,7 +35,7 @@ export default class LoginComponent extends Component {
                 </Form.Text>
                 </Form.Group>
             
-                <Form.Group controlId="formBasicPassword">
+                <Form.Group controlId="password">
                 <Form.Label>Password</Form.Label>
                 <Form.Control type="password" placeholder="Password" />
                 </Form.Group>
--- front/src/Components/RegisterComponent.js
+++ front/src/Components/RegisterComponent.js
@@ -6,28 +6,51 @@ import {Form, Button, Container} from 'react-bootstrap';
 export default class RegisterComponent extends Component {
   constructor(props) {
     super();
-    // this.state.handlePlayPause = this.state.handlePlayPause.bind(this)
+    this.onRegister = this.onRegister.bind(this)
     this.state = {
+      register: {
+        firstname: "",
+        lastname: "",
+        email: "",
+        password: "",
+      } 
     }
 }
-  
+  onRegister(event) {
+    event.preventDefault()
+    this.state.register.firstname = event.target.firstname.value
+    this.state.register.lastname = event.target.lastname.value
+    this.state.register.email = event.target.email.value
+    this.state.register.password = event.target.password.value
+    console.log(this.state.register)
+  }
   render() 
   {
     return (
         <Container className="login-container">
             <h1 className="register-title">Register</h1>
-            <Form>
-                <Form.Group controlId="formBasicEmail">
-                <Form.Label>Email address</Form.Label>
-                <Form.Control type="email" placeholder="Enter email" />
-                <Form.Text className="text-muted">
-                    We'll never share your email with anyone else.
-                </Form.Text>
+            <Form onSubmit={this.onRegister}>
+                <Form.Group controlId="firstname">
+                  <Form.Label>Firstname</Form.Label>
+                  <Form.Control placeholder="Enter Firstname" />
+                </Form.Group>
+
+                <Form.Group controlId="lastname">
+                  <Form.Label>Lastname</Form.Label>
+                  <Form.Control placeholder="Enter Lastname" />
+                </Form.Group>
+              
+                <Form.Group controlId="email">
+                  <Form.Label>Email address</Form.Label>
+                  <Form.Control type="email" placeholder="Enter Email" />
+                  <Form.Text className="text-muted">
+                      We'll never share your email with anyone else.
+                  </Form.Text>
                 </Form.Group>
             
-                <Form.Group controlId="formBasicPassword">
-                <Form.Label>Password</Form.Label>
-                <Form.Control type="password" placeholder="Password" />
+                <Form.Group controlId="password">
+                  <Form.Label>Password</Form.Label>
+                  <Form.Control type="password" placeholder="Password" />
                 </Form.Group>
                 <Button variant="dark" type="submit">
                 Register
