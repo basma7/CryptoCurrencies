f3fa265537da16e080fc63fb34b448808f25e1fa
titouan Le Floch Riche <titouan.leflochriche@epitech.eu>
2020-02-11 11:51:09 +0100

    Fix Crypto

--- config/db.js
+++ config/db.js
@@ -4,7 +4,7 @@ const pool = createPool({
     port: 3306,
     host: "localhost",
     user: "root", 
-    password: "password", 
+    password: "", 
     database: "test",
     connectionLimit: 10
   });
